5da48ee5b22a088e12f64cb2987b65fccaad5f8b
[Niranjan160}] <[niranjanmariyappan1@gmail.com]>
2024-11-27 08:04:45 +0530

    Footer changes , menu removed, responsive gallery section, header alignment changed

--- script.js
+++ script.js
@@ -9,17 +9,19 @@
     event.preventDefault();
     alert("Thank you for reaching out! We'll get back to you soon.");
   });  
-  // Show full-size image in modal
-function viewFullSize(image) {
-    const modal = document.getElementById("modal");
-    const fullImage = document.getElementById("fullImage");
-    modal.style.display = "flex"; // Show the modal
-    fullImage.src = image.src; // Set the clicked image as the modal image
-  }
+
+  // Function to open the image in full size
+function viewFullSize(element) {
+  var modal = document.getElementById("modal");
+  var modalImg = document.getElementById("fullImage");
   
-  // Close modal
-  function closeModal() {
-    const modal = document.getElementById("modal");
-    modal.style.display = "none"; // Hide the modal
-  }
-  
+  // Set the source of the modal image to the clicked image's source
+  modal.style.display = "flex";
+  modalImg.src = element.src;
+}
+
+// Function to close the modal when clicked outside the image
+function closeModal() {
+  var modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
